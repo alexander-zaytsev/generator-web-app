9a1aff0c50c02caf975a5d2295fbf476c21d13b4
Alexander Zaytsev <alz@tuta.io>
2016-04-17 14:34:28 +0300

    fix: Change arrow functions to regular, rename .jade to .pug

--- generators/app/index.js
+++ generators/app/index.js
@@ -6,15 +6,15 @@ const path = require('path');
 
 module.exports = yeoman.Base.extend({
 
-  prompting: () => {
-    const done = this.async();
+  prompting: function () {
+    let done = this.async();
 
     // Have Yeoman greet the user.
     this.log(yosay(
       'Welcome to the supreme ' + chalk.red('generator-web-app') + ' generator!'
     ));
 
-    var prompts = [{
+    const prompts = [{
       type: 'input',
       name: 'projectName',
       message: 'Enter the name of your project'
@@ -30,24 +30,25 @@ module.exports = yeoman.Base.extend({
       default: true
     }];
 
-    this.prompt(prompts, (props) => {
+    this.prompt(prompts, function (props) {
       this.props = props;
       // To access props later use this.props.someAnswer;
 
       done();
-    });
+    }.bind(this));
   },
 
   writing: {
-    fixed: () => {
+    fixed: function () {
       const fixedFilePaths = [
         'main.js', 'colorScheme.less',
         'global.less',  'interface.less', 'layout.less',
         'type.less', 'variables.less', 'README.md', '.editorconfig',
-        '.gitignore', 'travis.yml'
+        '.gitignore', '.travis.yml'
       ];
 
-      fixedFilePaths.forEach( (fromFilePath) => {
+      fixedFilePaths.forEach( function (fromFilePath) {
+
         let toFilePath =
           path.join(this.props.projectName, fromFilePath);
         if (/.*\.less$/.test(fromFilePath)) {
@@ -56,23 +57,21 @@ module.exports = yeoman.Base.extend({
         } else if (fromFilePath === 'main.js') {
           toFilePath = path.join(this.props.projectName + 'app',
             'scripts', fromFilePath);
-        } else if (fromFilePath === 'package.json') {
-          toFilePath = path.join(this.props.projectName, fromFilePath);
         }
 
         this.fs.copy(
           this.templatePath(fromFilePath),
           this.destinationPath(toFilePath)
         );
-      });
+      }.bind(this));
     },
 
-    flexible: () => {
+    flexible: function () {
       const flexibleFilePaths = [
-        'index.jade', 'bower.json', 'package.json', 'main.less',
+        'index.pug', 'bower.json', 'package.json', 'main.less',
         'gulpfile.js'
       ];
-      flexibleFilePaths.forEach(function (fromFilePath) {
+      flexibleFilePaths.forEach( function (fromFilePath) {
         let toFilePath =
           path.join(this.props.projectName, fromFilePath);
         if (fromFilePath === 'index.pug') {
@@ -91,31 +90,26 @@ module.exports = yeoman.Base.extend({
       }.bind(this));
     },
 
-    optional: () => {
+    optional: function () {
       const optionalFileNames = [
         'normalize.less'
       ];
 
-      optionalFileNames.forEach((fromFilePath) => {
+      optionalFileNames.forEach( function (fromFilePath) {
         let toFilePath =
           path.join(this.props.projectName, fromFilePath);
         if (fromFilePath === 'normalize.less' &&
-            this.props.includeNormalize === true ) {
+          this.props.includeNormalize === true ) {
 
-          toFilePath =
-            path.join(this.props.projectName, 'app', 'styles', 'vendor',
-              'fromFilePath');
+          toFilePath = path.join(this.props.projectName, 'app',
+            'styles', 'vendor', 'fromFilePath');
           this.fs.copy(
             this.templatePath(fromFilePath),
             this.destinationPath(toFilePath)
           );
 
         }
-      };
+      }.bind(this));
     }
-  },
-
-  install: () => {
-    this.installDependencies();
   }
 });
